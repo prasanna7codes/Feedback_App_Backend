bfa60c16ff918e07db4a32e22ef825c2199e8db6
prasanna7codes <prasannalit098@gmail.com>
2025-06-27 13:15:30 +0530

    writing the auth route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import ConnectDB from './config/db';
 
+import AuthRoutes from './routes/auth'
+
 
 
 ConnectDB();
@@ -10,6 +12,15 @@ const app = express();
 
 
 app.use(express.json());
+
+
+
+app.use('/api/auth',AuthRoutes)
+
+
+
+app.listen(3000)
+
 
 
 
